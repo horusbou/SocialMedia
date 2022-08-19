829aefd5990fc06f29285e4a6b610f4b6cb83799
horusbou <hamza.bouqal@edu.uiz.ac.ma>
2022-08-19 04:43:49 +0100

    Component to render if there is no Tweets on users account

---
+++ client/src/compontents/NoTweets/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+export default function NoTweetFound(){
+    return <div className='notweetfound'>
+        <div className="content">
+            <span className="waving-hand">👋</span>
+            <div className="text">
+                <h1>Try to Tweet your first Tweet</h1>
+            </div>
+        </div>
+    </div>
+}
